44e15338b2714fdb27ad9ec03d3f54bedd0aa248
viictv <officialviic@gmail.com>
2026-01-29 15:44:06 +0100

    exercises done

--- 3._Objects.js
+++ 3._Objects.js
@@ -7,16 +7,24 @@ const greetings = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
+console.log(greetings.message);
+
 // --------------------------------------
 // Exercise 2 - Defining an object.
 
 // Create an object that has your name and age. 
 
+const me = {
+    name: "Victor",
+    age: 29
+}
 
 // --------------------------------------
 // Exercise 3 - Add a key-value pair
 
-const stackOverflow = {};
+const stackOverflow = {
+    isAllowed: true
+};
 
 // make a rule called isAllowed and let the value be true
 
@@ -27,6 +35,8 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.about = "Just a tribute.";
 
 // --------------------------------------
 
